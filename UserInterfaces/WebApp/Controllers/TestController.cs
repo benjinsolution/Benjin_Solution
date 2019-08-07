3611cac76871dae8e6e29162bc74876b94fa682e
zouql <Thinkxuhao@outlook.com>
2019-08-07 13:43:12 +0800

    新增Post测试

--- UserInterfaces/WebApp/Controllers/TestController.cs
+++ UserInterfaces/WebApp/Controllers/TestController.cs
@@ -1,9 +1,13 @@
 ﻿namespace WebApp.Controllers
 {
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Web;
     using System.Web.Http;
     using Application.Tests;
     using Application.Tests.Models;
+    using Newtonsoft.Json;
 
     [AllowAnonymous]
     public class TestController : ApiController
@@ -31,6 +35,34 @@
         public IHttpActionResult CreateOrUpdate(TestBindModel model)
         {
             return Ok(appService.CreateOrUpdate(model));
+        }
+
+        [HttpPost]
+        public IHttpActionResult TestPost()
+        {
+            var request = HttpContext.Current.Request;
+
+            var model = JsonConvert.DeserializeObject<Model>(request.Params["PostData"]);
+
+            var files = request.Files.AllKeys.Select(m =>
+            {
+                var ms = new MemoryStream();
+
+                var file = request.Files.Get(m);
+
+                file.InputStream.CopyTo(ms);
+
+                return new { file.FileName, Stream = ms };
+            }).ToDictionary(k => k.FileName, v => v.Stream);
+
+            return Ok(new { model, files = files.Select(m => new { fileName = m.Key, fileLength = m.Value.Length }) });
+        }
+
+        public class Model
+        {
+            public string Name { get; set; }
+
+            public string IdNo { get; set; }
         }
     }
 }
